731a06a738db132992ada9e88cfa17cb25d96d36
PhanNgocTrieu <47733751+PhanNgocTrieu@users.noreply.github.com>
2021-04-13 22:07:02 +0700

    Create Subcriber.py

---
+++ NHAYENDEMO/Subcriber.py
@@ -0,0 +1,20 @@
+# Test:
+
+import paho.mqtt.client as paho
+
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" " + str(msg.qos) + " " + str(msg.payload))
+
+
+client = paho.Client()
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.connect("broker.hivemq.com", 1883)
+client.subscribe([("esp/bme680/temperature", 0), ("esp/bme680/humidity", 0)])
+
+client.loop_forever()
