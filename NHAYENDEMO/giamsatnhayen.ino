c257be600c6026f5bc7b4b900e53d48aee68d5c0
PhanNgocTrieu <47733751+PhanNgocTrieu@users.noreply.github.com>
2021-04-21 17:34:27 +0700

    Update giamsatnhayen.ino

--- NHAYENDEMO/giamsatnhayen.ino
+++ NHAYENDEMO/giamsatnhayen.ino
@@ -1,10 +1,10 @@
 #ifdef ESP32
-  #include <WiFi.h>
-  #include <HTTPClient.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #else
-  #include <ESP8266WiFi.h>
-  #include <ESP8266HTTPClient.h>
-  #include <WiFiClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #endif
 
 /* Libraries for MQTT */
@@ -17,60 +17,164 @@
 #include <Adafruit_MQTT.h>
 #include <Adafruit_MQTT_Client.h>
 #include <ArduinoJson.h>
+#include <AsyncMqttClient.h>
+#include <Ticker.h>
+
+
+
+#define WIFI_SSID "UNT8 CAFE tang2"
+#define WIFI_PASSWORD "08laclongquan"
+
+
+// for using MQTT
+#define MQTT_HOST IPAddress(192,168,0,102) // // 192.168.0.102
+#define MQTT_PORT 1883
+
+// Temperature MQTT Topics
+#define MQTT_PUB_TEMP "esp/trieu/temperature"
+#define MQTT_PUB_HUM  "esp/trieu/humidity"
+
+// Subscribe topic:
+#define MQTT_SUB_WATERPUMP "esp/trieu/D8"
+#define MQTT_SUB_SPINNING "esp/trieu/D7"
+
+#define MQTT_SUB_AUTO "esp/trieu/auto"
+
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+
+Ticker wifiReconnectTimer;
 
 
 /*======================== Defination of PIN ================================*/
 // D1 is output of reading temperature and humidity
 #define DHTPIN D1
+#define WATER_PUMPER_PIN 15
 
 
-/*************************** Sketch Code ************************************/
+/*======================== Defination of SOMETHINGS =========================*/
 
-// Replace with your network credentials
-const char* ssid     = "CAO COFFEE & OFFICE"; // "Le An"
-const char* password = "68686868"; // "hohoa46481990"
- 
-// REPLACE with your Domain name and URL path or IP address with path
-const char* serverName = "http://192.168.1.8/esp_data.php";
- 
-//const int oneWireBus = 4;
-// Keep this API Key value to be compatible with the PHP code provided in the project page. 
-// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
-String apiKeyValue = "kjsjkhjdhfd";
- 
 String sensorName = "DHT11";
 String sensorLocation = "home";
-float temperatureC = 0.0;
-float humidity = 0.0;  
+float temperature = 0.0;
+float humidity = 0.0;
+
+
+
+/* ====================== MQTT's functions ==========================*/
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+
+  // Subscribe
+  uint16_t packetIdSub = mqttClient.subscribe("esp/trieu/D8", 2); // nhận => subcribe
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub);
+
+  // publish
+  //  mqttClient.publish("test/lol", 0, true, "test 1");
+  //  Serial.println("Publishing at QoS 0");
+  //  uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
+  //  Serial.print("Publishing at QoS 1, packetId: ");
+  //  Serial.println(packetIdPub1);
+  //  uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
+  //  Serial.print("Publishing at QoS 2, packetId: ");
+  //  Serial.println(packetIdPub2);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+
+  if (strcmp(topic,MQTT_SUB_WATERPUMP) == 0)
+  {
+    char* MQTT_SUB_WATERPUMP_ON = "esp/trieu/D8/on";
+    char* MQTT_SUB_WATERPUMP_OFF = "esp/trieu/D8/off";
+    if (strcmp(payload,MQTT_SUB_WATERPUMP_ON) == 0)
+    {
+      digitalWrite(WATER_PUMPER_PIN, HIGH);
+    }
+    if (strcmp(payload,MQTT_SUB_WATERPUMP_OFF) == 0)
+    {
+      digitalWrite(WATER_PUMPER_PIN, LOW);
+    }
+  }
+  
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+/* ==================== END OF MQTT's Declaration =============================*/
+
+
+
+/* ====== Starting DHT =========*/
 DHT dht(DHTPIN, DHT11);
-
-
-
-/************************* Adafruit.io Setup *********************************/
-
-#define SERVER    "127.0.0.1" 
-#define PORT      1883
-#define USERNAME  "root"
-#define PASSWORD  ""
-//#define DEVICE_ID "Xxs2h2"
-
-/************ Global State (you don't need to change this!) ******************/
-
-// Create a BridgeClient instance to communicate using the Yun's bridge & Linux OS.
-BridgeClient client;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, SERVER, PORT, USERNAME, PASSWORD);
-
-
-/****************************** Feeds ***************************************/
-
-// Setup a feed called 'assetPub' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish assetPub = Adafruit_MQTT_Publish(&mqtt, "up/client/" USERNAME);
-
-// Setup a feed called 'assetSub' for subscribing to changes.
-Adafruit_MQTT_Subscribe assetSub = Adafruit_MQTT_Subscribe(&mqtt, "down/light/" USERNAME);
 
 
 float getTemperature()
@@ -86,180 +190,103 @@ float getHumidity()
   float hum = dht.readHumidity();
   if (isnan(hum))
     Serial.println("reading hum failed");
-  
+
   return hum;
 }
 
-int LIGHTPIN = 15; //D8
-String sPayload;
-char* cPayload;
-
-void MQTT_connect() {
- int8_t ret;
-
- // Stop if already connected.
- if (mqtt.connected()) {
-   return;
- }
-
- Console.print("Connecting to MQTT... ");
-
- while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-      Console.println(mqtt.connectErrorString(ret));
-      Console.println("Retrying MQTT connection in 5 seconds...");
-      mqtt.disconnect();
-      delay(5000);  // wait 5 seconds
- }
- Console.println("MQTT Connected!");
-}
-
 void setup() {
+  /* ========== MQTT ===========*/
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+  /* ========== MQTT ===========*/
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish); // Call Publish function
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT); // nap lai?
 
-  // initialize digital pin LED_BUILTIN as an output.
-  //pinMode(LED_BUILTIN, OUTPUT);
 
-
-
-
-  /* ========== HTTP ===========*/
+  // connecting wifi pin:
   pinMode(16, OUTPUT);
   pinMode(2, OUTPUT);
+
+
+  // WATER_PUMPER_PIN:
+  pinMode(WATER_PUMPER_PIN, OUTPUT);
+
+
+
   digitalWrite(16, LOW); // chưa kết nối;
-   
+
+
+
   Serial.begin(115200);
 
 
   // Start the DHT sensor
   dht.begin();
-  
-  WiFi.begin(ssid, password);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Connecting");
-  
-  while(WiFi.status() != WL_CONNECTED) { 
+
+  while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(2, HIGH);
     delay(250);
     Serial.println(".");
-    digitalWrite(2, LOW); 
+    digitalWrite(2, LOW);
     delay(250);
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
 
-  pinMode(LIGHTPIN, OUTPUT); // D8 is output pin}
 
-  
-  /* =================== MQTT ============*/
-  Bridge.begin();
-  Console.begin();
-  
-  // Setup MQTT subscription for onoff feed.
-  mqtt.subscribe(&assetSub);
 }
 
 void loop() {
-  //Check WiFi connection status
-  if(WiFi.status()== WL_CONNECTED){
-    digitalWrite(16, HIGH);  // đã kết nối
-    HTTPClient http;
 
-    http.begin(serverName);
-    //sensors.requestTemperatures();
-    
+
+  //Check WiFi connection status
+  if (WiFi.status() == WL_CONNECTED) {
+    digitalWrite(16, HIGH);  // đã kết nối
+
     humidity = getHumidity();
-    
+
     // Read temperature as Celsius (the default)
-    temperatureC = getTemperature();
-    
-    // Specify content-type header
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    
-    // Prepare your HTTP POST request data
-    String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName + "&location=" + sensorLocation + "&temp=" + temperatureC + "&hum="+ humidity;
-    
-    Serial.print("httpRequestData: ");
-    Serial.println(httpRequestData);
-    
-    // Send HTTP POST request
-    
-    int httpResponseCode = http.POST(httpRequestData);
-    
-    String payload = http.getString();
-        
-    if (httpResponseCode>0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      Serial.println(payload);  
-    }
-    else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
-    }
-    // Free resources
-    http.end();
+    temperature = getTemperature();
+
     //Send an HTTP POST request every 6 seconds
-    delay(60000);  // 60s
+    delay(6000);  // 60s
   }
   else {
     Serial.println("WiFi Disconnected");
     digitalWrite(2, HIGH);
     delay(250);
     Serial.print(".");
-    digitalWrite(2, LOW); 
+    digitalWrite(2, LOW);
     delay(250);
   }
 
-  /*=========== MQTT ==========*/
+  // Subscribe an MQTT message on topic esp/trieu/D8
+  uint16_t packetIdSub = mqttClient.subscribe(MQTT_SUB_WATERPUMP, 1); // nhận => subcribe
+  Serial.printf("Subscribing on topic %s at QoS 2, packetId: %i \n", MQTT_SUB_WATERPUMP, packetIdSub);
+  
 
-  MQTT_connect();
-  
-  /* ======= PUBLISHING ====== */
-  /*
-  analogReference(EXTERNAL);
-  pinMode(12,OUTPUT);
 
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& payload = jsonBuffer.createObject();
-  payload["device_id"] = DEVICE_ID;
-  payload["lightlevel"] = getTemperature();
-  
-  
-  sPayload = "";
-  payload.printTo(sPayload);
-  cPayload = &sPayload[0u];
-  
-  // Now we can publish stuff!
-  Console.print(F("\nPublishing "));
-  Console.print(cPayload);
-  Console.print("...");
-  
-  if (! assetPub.publish(cPayload)) {
-  Console.println(F("Failed"));
-  } else {
-  Console.println(F("OK!"));
+
+
+
+
+
+    // Publish an MQTT message on topic esp/trieu/temperature
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temperature).c_str());
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEMP, packetIdPub1);
+    Serial.printf("Message: %.2f \n", temperature);
+
+    // Publish an MQTT message on topic esp/trieu/humidity
+    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(humidity).c_str());
+    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+    Serial.printf("Message: %.2f \n", humidity);
   }
-  */
-
-  /* ============ Subcriber ==============*/
-  // this is our 'wait for incoming subscription packets' busy subloop
-//  Adafruit_MQTT_Subscribe *subscription;
-//  while ((subscription = mqtt.readSubscription(300))) {
-//    if (subscription == &assetSub) {
-//     Console.print(F("Got: "));
-//     Console.println((char *)assetSub.lastread);
-//    
-//     if (strcmp((char *)assetSub.lastread, "ON") == 0) {
-//       digitalWrite(LIGHTPIN, HIGH);
-//     }
-//     if (strcmp((char *)assetSub.lastread, "OFF") == 0) {
-//       digitalWrite(LIGHTPIN, LOW);
-//     }
-//    }
-//  }
-//  
-//  // ping the server to keep the mqtt connection alive
-//  if(! mqtt.ping()) {
-//  Console.println(F("MQTT Ping failed."));
-//  }
-//  delay(9000);  // wait 9 seconds
-}
